5e138e865eb4549a3185f24ace67edda47d6abcb
arnoldatk94 <arnoldatk94@gmail.com>
2023-03-01 19:29:09 +0800

    working upload chat system

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { getDatabase, onChildAdded, push, ref, set } from "firebase/database";
-import { database } from "./firebase";
+import { database, storage } from "./firebase";
+import {
+  getDownloadURL,
+  getStorage,
+  ref as storageRef,
+  uploadBytes,
+} from "firebase/storage";
 import logo from "./logo.png";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,6 +14,8 @@ import { ListGroup, Button, Form, Container, Row } from "react-bootstrap";
 
 // Save the Firebase message folder name as a constant to avoid bugs due to misspelling
 const DB_MESSAGES_KEY = "messages";
+// Saving firebase folder for image storage
+const STORAGE_FILE_KEY = "images";
 
 class App extends React.Component {
   constructor(props) {
@@ -18,6 +26,9 @@ class App extends React.Component {
       messages: [],
       input: "",
       timestamp: "",
+      fileInputFile: null,
+      fileInputValue: "",
+      textInputValue: "",
     };
   }
 
@@ -44,27 +55,73 @@ class App extends React.Component {
     });
   }
 
+  handleFileChange = (e) =>
+    this.setState({
+      fileInputFile: e.target.files[0],
+      fileInputValue: e.target.value,
+    });
+
+  handleSubmit = (e) => {
+    // Creates a reference to the bucket and save to storage
+    const imageRef = storageRef(
+      storage,
+      `${STORAGE_FILE_KEY}/${this.state.fileInputFile.name}`
+    );
+
+    // This block is completely redundant for storage
+    // const newImageRef = push(imageRef);
+    // set(newImageRef, {
+    //   file: this.state.fileInputFile,
+    // });
+
+    uploadBytes(imageRef, this.state.fileInputFile).then((snapshot) => {
+      console.log(snapshot);
+      console.log(this.state);
+      getDownloadURL(imageRef, this.state.fileInputFile).then((url) => {
+        console.log("URL", url);
+
+        this.writeData(url);
+      });
+    });
+  };
+
   // Note use of array fields syntax to avoid having to manually bind this method to the class
-  writeData = () => {
+  writeData = (url) => {
     const messageListRef = ref(database, DB_MESSAGES_KEY);
     const newMessageRef = push(messageListRef);
     set(newMessageRef, {
       message: this.state.input,
       timestamp: this.state.timestamp,
+      url: url,
+    });
+    this.setState({
+      input: "",
+      timestamp: "",
+      fileInputFile: null,
+      fileInputValue: "",
+      textInputValue: "",
     });
   };
 
   render() {
     // Convert messages in state to message JSX elements to render
-
+    console.log(this.state.fileInputFile);
     let messageListItems = this.state.messages.map((message) => (
       <div className="container" key={message.key}>
         <div className="timestamp">{message.val.timestamp}</div>
         <div className="message">{message.val.message}.</div>
+        <div className="picture">
+          {message.val.url ? (
+            <img src={message.val.url} alt={message.val.url} width="50%" />
+          ) : (
+            "No image"
+          )}
+        </div>
       </div>
     ));
 
-    const disableInput = this.state.input.length <= 1;
+    const disableInput =
+      this.state.input.length <= 1 || this.state.fileInputFile == null;
     return (
       <div className="App">
         <header className="App-header">
@@ -72,18 +129,22 @@ class App extends React.Component {
           <div className="flex-container">
             <Form.Group>
               <Form.Control
+                type="file"
+                value={this.state.fileInputValue}
+                onChange={this.handleFileChange}
+              ></Form.Control>
+              <Form.Control
                 type="text"
                 name="input"
-                placeholder="Send chat"
+                placeholder="Send Post"
                 value={this.state.input}
                 onChange={this.handleChange}
               />
             </Form.Group>
-
             <Button
               disabled={disableInput}
               variant="success"
-              onClick={this.writeData}
+              onClick={this.handleSubmit}
             >
               Send
             </Button>
